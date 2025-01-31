3f32d1414c90d90b2de8fadbcbfea3fe5842cd22
Jerry Wang <05jwang@gmail.com>
2025-01-31 10:09:54 -0500

    feat: models for dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    discription = models.TextField()
+    image = models.URLField()
+    release_date = models.DateField()
+    genres = models.ManyToManyField("Genre", related_name="movies")
+
+    def __str__(self):
+        return self.title
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
