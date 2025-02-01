6a210b48aee80e6403e68a3267b718fa34c2d45a
k3sharma <kush3sharma@gmail.com>
2025-01-31 23:31:41 -0500

    signup page

--- moviestore/settings.py
+++ moviestore/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'accounts' # adding accounts app to movie file
 ]
 
 MIDDLEWARE = [
--- moviestore/urls.py
+++ moviestore/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls')), # accounts url
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
