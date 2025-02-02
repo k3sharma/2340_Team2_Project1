32e069d9c031c46acb0f44d2c8dbfd725f1cbcbf
JoshuaJ <jkjoseph0629@gmail.com>
2025-02-02 13:40:57 -0500

    Searching feature implemented as well as minor aesthetic changes.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.movie_dashboard, name='movie_dashboard'),
     path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('about/', views.about, name='about'),
+<<<<<<< HEAD
 
+=======
+    path('search/', views.search, name='search'),
+>>>>>>> b882f04 (Searching feature implemented as well as minor aesthetic changes.)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
