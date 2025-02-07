9b9a0c2c6172f69eeaf19992125a0dc09e0ef966
JoshuaJ <jkjoseph0629@gmail.com>
2025-02-07 14:11:03 -0500

    Implemented ability for users to edit and delete their reviews.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('search/', views.search, name='search'),
     path('<int:movie_id>/review/create/', views.create_review, name='create_review'),
+    path('<int:movie_id>/review/<int:review_id>/', views.edit_review, name='edit_review'),
+    path('<int:movie_id>/review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
