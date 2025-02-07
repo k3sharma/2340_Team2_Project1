6b0de01dde4d55b32839f0aabf0cf952c84321fb
Jerry Wang <05jwang@gmail.com>
2025-02-07 12:02:00 -0500

    feat: added checkout functionality

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('', views.index, name='cart.index'),
     path('<int:id>/add/', views.add, name='cart.add'),
     path('clear/', views.clear, name='cart.clear'),
+    path('<int:id>/remove/', views.remove, name='cart.remove'), # FIXME: this is not used
+    path('orders/', views.orders, name='cart.orders'),
+    path('checkout/', views.checkout, name='cart.checkout'),
 ]
