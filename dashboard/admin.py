b72ecc78b7b5a8219d5bdd264a2cb05ec06f3cc9
Jerry Wang <05jwang@gmail.com>
2025-02-07 10:26:45 -0500

    feat: added order schema in database

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Genre, Movie, MovieGenre, Review
+from .models import Genre, Movie, MovieGenre, Review, Order, OrderItem
+
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
@@ -16,4 +17,8 @@ class MovieAdmin(admin.ModelAdmin):
 
 admin.site.register(Genre)
 admin.site.register(MovieGenre)
+
 admin.site.register(Review)
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
